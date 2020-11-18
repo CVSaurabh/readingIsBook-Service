46d641c1b852e023f082d8af1768d5cba29cbffa
CVSaurabh <cvsaurabh26@gmail.com>
2020-11-18 18:31:00 +0530

    initial commit

---
+++ ReadingIsGood/src/main/java/com/example/ReadingIsGood/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.example.ReadingIsGood.repository;
+
+import java.util.Optional;
+
+import com.example.ReadingIsGood.models.ERole;
+import com.example.ReadingIsGood.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
